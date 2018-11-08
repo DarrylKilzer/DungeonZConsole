4c919d946328df292aac73268ec2468616ae77fd
Darryl Kilzer <darrylkilzer@gmail.com>
2018-11-08 14:56:17 -0700

    Never move things in folders in .NET...

---
+++ DungeonZ/Core/Interfaces/IActor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonZ.Core.Interfaces
+{
+    interface IActor
+    {
+        string Name { get; set; }
+        int Awareness { get; set; }
+    }
+}
