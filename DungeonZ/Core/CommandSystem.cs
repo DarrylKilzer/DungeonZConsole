d4b872ef60c3ee2d5ccb8c97dc9ec4b0358d3bb6
Darryl Kilzer <darrylkilzer@gmail.com>
2018-11-11 09:45:48 -0700

    damage system works to attack monsters

--- DungeonZ/Core/CommandSystem.cs
+++ DungeonZ/Core/CommandSystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RogueSharp.DiceNotation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +15,6 @@ namespace DungeonZ.Core
         {
             int x = Game.Player.X;
             int y = Game.Player.Y;
-            Monster monster = Game.DungeonMap.GetMonsterAt(x, y);
-
-            if (monster != null)
-            {
-                Attack(Game.Player, monster);
-                return true;
-            }
 
             switch (direction)
             {
@@ -61,9 +55,130 @@ namespace DungeonZ.Core
                 return true;
             }
 
+            //if the first action failed, maybe there is a monster, try to get it
+            Monster monster = Game.DungeonMap.GetMonsterAt(x, y);
+
+            //if there is a monster attack it
+            if (monster != null)
+            {
+                Attack(Game.Player, monster);
+                return true;
+            }
+
+            //otherwise its a non walkable position, fail
             return false;
         }
 
+        public void Attack(Actor attacker, Actor defender)
+        {
+            StringBuilder attackMessage = new StringBuilder();
+            StringBuilder defenseMessage = new StringBuilder();
 
+            int hits = ResolveAttack(attacker, defender, attackMessage);
+
+            int blocks = ResolveDefense(defender, hits, attackMessage, defenseMessage);
+
+            Game.MessageLog.Add(attackMessage.ToString());
+            if (!string.IsNullOrWhiteSpace(defenseMessage.ToString()))
+            {
+                Game.MessageLog.Add(defenseMessage.ToString());
+            }
+
+            int damage = hits - blocks;
+
+            ResolveDamage(defender, damage);
+        }
+
+        // The attacker rolls based on his stats to see if he gets any hits
+        //this is currently set up much like Descent the board game determine number of hits that will
+        //deal damage and the number of defenses made to remove those damages.
+        private static int ResolveAttack(Actor attacker, Actor defender, StringBuilder attackMessage)
+        {
+            int hits = 0;
+
+            attackMessage.AppendFormat("{0} attacks {1} and rolls: ", attacker.Name, defender.Name);
+
+            // Roll a number of 100-sided dice equal to the Attack value of the attacking actor
+            DiceExpression attackDice = new DiceExpression().Dice(attacker.Attack, 100);
+            DiceResult attackResult = attackDice.Roll();
+
+            foreach (TermResult termResult in attackResult.Results)
+            {
+                attackMessage.Append(termResult.Value + ", ");
+                if (termResult.Value >= 100 - attacker.AttackChance)
+                {
+                    hits++;
+                }
+            }
+
+            return hits;
+        }
+
+        // The defender rolls based on his stats to see if he blocks any of the hits from the attacker
+        //functions just like attack and Descent
+        private static int ResolveDefense(Actor defender, int hits, StringBuilder attackMessage, StringBuilder defenseMessage)
+        {
+            int blocks = 0;
+
+            if (hits > 0)
+            {
+                attackMessage.AppendFormat("scoring {0} hits.", hits);
+                defenseMessage.AppendFormat("  {0} defends and rolls: ", defender.Name);
+
+                DiceExpression defenseDice = new DiceExpression().Dice(defender.Defense, 100);
+                DiceResult defenseRoll = defenseDice.Roll();
+
+                foreach (TermResult termResult in defenseRoll.Results)
+                {
+                    defenseMessage.Append(termResult.Value + ", ");
+                    if (termResult.Value >= 100 - defender.DefenseChance)
+                    {
+                        blocks++;
+                    }
+                }
+                defenseMessage.AppendFormat("resulting in {0} blocks.", blocks);
+            }
+            else
+            {
+                attackMessage.Append("and misses completely.");
+            }
+
+            return blocks;
+        }
+
+        // Apply any damage that wasn't blocked to the defender
+        private static void ResolveDamage(Actor defender, int damage)
+        {
+            if (damage > 0)
+            {
+                defender.Health = defender.Health - damage;
+
+                Game.MessageLog.Add($"  {defender.Name} was hit for {damage} damage");
+
+                if (defender.Health <= 0)
+                {
+                    ResolveDeath(defender);
+                }
+            }
+            else
+            {
+                Game.MessageLog.Add($"  {defender.Name} blocked all damage");
+            }
+        }
+
+        // Remove the defender from the map and add some messages upon death.
+        private static void ResolveDeath(Actor defender)
+        {
+            if (defender is Player)
+            {
+                Game.MessageLog.Add($"  {defender.Name} was killed, GAME OVER!");
+            }
+            else if (defender is Monster)
+            {
+                Game.DungeonMap.RemoveMonster((Monster)defender);
+
+                Game.MessageLog.Add($"  {defender.Name} died and dropped {defender.Gold} gold");
+            }
+        }
     }
 }
